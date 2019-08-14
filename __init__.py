dacffdd6bab8233777c1e3daf54c8fb727adb6af
Christopher DiTusa <cditusa@gmail.com>
2019-08-14 16:13:51 -0400

    Gets the first page of results

--- __init__.py
+++ __init__.py
@@ -1,13 +1,34 @@
+#!/usr/bin/env python3
+
 import os, requests, logging, argparse
 from urllib import parse
 from bs4 import BeautifulSoup
+import re
 
-class Lead(object):
-    pass
+class Lead():
+    def __init__(self, name, phone, street, locale, category):
+        self.name = name
+        self.category = category
+        self.phone = phone
+        self.street = street
+        self.parse_locale(locale)
+    
+    def parse_locale(self, locale):
+        if locale is not None:
+            regex = re.compile(r'(.*),\s(\w{2})\s(\d{5})')
+            match = regex.match(locale)
+            self.city = match.groups()[0]
+            self.state = match.groups()[1]
+            self.postal = match.groups()[2]
+        else:
+            self.city, self.state, self.postal = '', '', ''
 
-class WebPage(object):
-    def __init__(self, uri='https://yellowpages.com'):
+
+class WebPage():
+    def __init__(self, keyword, location, uri='https://yellowpages.com'):
         self.uri = uri
+        self.keyword = keyword
+        self.location = location
         self.logger = logging.getLogger('scrape.webpage')
 
     def _get_uri(self, search= {}):
@@ -15,14 +36,20 @@ class WebPage(object):
         if search:
             uri += '/search?{}'.format(parse.urlencode(search))
             self.logger.debug(uri)
-        return uri
+        return uri        
 
-    def get(self, keyword, location):
+    def _get_response(self, keyword, location):
         search_terms = {'search_terms': keyword, 'geo_location_terms': location}
         uri = self._get_uri(search_terms)
         response = requests.get(uri)
         response.raise_for_status()
         return response
+
+    def parse_results(self):
+        response = self._get_response(keyword=self.keyword, location=self.location)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        return soup.find_all(id=re.compile('^lid'))
+
 
 
 def main():
@@ -42,11 +69,23 @@ def main():
         log_level = logging.DEBUG
     logging.basicConfig(level=log_level)
     logger = logging.getLogger('scrape.main')
-
-    #search = {'search_terms': args.keyword, 'geo_location_terms': args.location}
     
-    web_page = WebPage()
-    response = web_page.get(keyword=args.keyword, location=args.location)
+    lead_list = []
+    web_page = WebPage(keyword=args.keyword, location=args.location)
+    results = web_page.parse_results()
+    for result in results:
+        name = result.find(class_='business-name').string if result.find(
+            class_='business-name') else ''
+        category = ','.join([x for x in result.find(class_='categories').strings])
+        phone = result.find(class_='phone').string if result.find(
+            class_='phone') else ''
+        street = result.find(class_='street-address').string if result.find(
+            class_='street-address') else ''
+        locale = result.find(class_='locality').string if result.find(
+            class_='locality') else ''
+        lead = Lead(name=name, phone=phone, street=street, locale=locale,
+                    category=category)
+        lead_list.append(lead)
 
     
 if __name__ == '__main__':
